7ff0945b570278fbb501d132c8abda6d00fe8b6f
CBer35 <97758741+CBer35@users.noreply.github.com>
2024-06-17 05:22:36 -0400

    Update install-server-lac.sh

--- install-server-lac.sh
+++ install-server-lac.sh
@@ -6,7 +6,9 @@ cd MA
 mkdir LAC
 cd LAC
 wget https://lacrimesonline.com/builds/LAC/ServerConfig.txt
-cd /workspaces/codespaces-blank/LacBash
+cd /$HOME
+mkdir LACINSTALL
+cd LACINSTALL
 wget https://lacrimesonline.com/builds/LAC/LAC_Linux_v1.7.1.zip
 unzip LAC_Linux_v1.7.1.zip
 chmod +x LAC_Linux_v1.7.1.x86_64
